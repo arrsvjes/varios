cefae1944fb6fc1c7074e45b48e6d9e77d37ee8a
arrsvjes <88336300+arrsvjes@users.noreply.github.com>
2022-08-03 08:08:17 -0500

    Update create-hook.sh

--- create-hook.sh
+++ create-hook.sh
@@ -3,3 +3,6 @@
 source /usr/local/etc/ocp4.config
 oc set build-hook bc/hook --post-commit --command -- \
     bash -c "\"curl -s -S -i -X POST http://builds-for-managers-${RHT_OCP4_DEV_USER}-post-commit.${RHT_OCP4_WILDCARD_DOMAIN}/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
+
+oc set build-hook bc/hook --post-commit --command -- \
+    bash -c "\"curl -s -S -i -X POST http://buildformanagers-toorrm-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
