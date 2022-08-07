a0ddaf0d687bbaf38efd1bc05872cc98c8e09741
arrsvjes <88336300+arrsvjes@users.noreply.github.com>
2022-08-07 11:45:54 -0500

    Update kill.sh

--- probes/kill.sh
+++ probes/kill.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source /usr/local/etc/ocp4.config
+APP_URL=$(oc get route probes -o jsonpath='{..spec.host}{"\n"}')
 
-APP_URL=probes-${RHT_OCP4_DEV_USER}-probes.${RHT_OCP4_WILDCARD_DOMAIN}
 curl http://$APP_URL/flip?op=kill-health
 curl http://$APP_URL/flip?op=kill-ready
